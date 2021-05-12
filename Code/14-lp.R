a9d7b6e2b377f76f251615cf5a05841d46afacf6
Anne296 <anne.buettgen@web.de>
2021-05-12 10:06:12 +0200

    chapter linear programming start

---
+++ Code/14-lp.R
@@ -0,0 +1,17 @@
+# The following code is taken from the fourth chapter of the online script, which provides more detailed explanations:
+# 
+
+#-------------------------------------------------------------------#
+#---------------------Install missing packages----------------------#
+#-------------------------------------------------------------------#
+
+# At the top of each script this code snippet will make sure that all required packages are installed
+## ------------------------------------------------------------------------
+req_packages <- c("Hmisc", "ggplot2", "car", "psych", "GPArotation","hornpa")
+req_packages <- req_packages[!req_packages %in% installed.packages()]
+lapply(req_packages, install.packages)
+
+
+#-------------------------------------------------------------------#
+#-------------------Linear Programming--------------------#
+#-------------------------------------------------------------------#
